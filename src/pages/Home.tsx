63e2874c3a44b279a53e3dfe8914b736fa596aa1
Christian Torres <christiantorresbz@gmail.com>
2025-11-07 07:07:50 -0600

    style: line up trending chips after categories

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -365,10 +365,12 @@ export default function Home() {
 
           {/* Desktop layout: categories left, trending right */}
           <div className="hidden md:flex items-center gap-3 max-w-[1800px] mx-auto">
-            <div className="flex items-center gap-3 flex-1">{categoryButtons}</div>
-            {hasTrending && (
-              <div className="flex items-center gap-3 ml-6">{renderTrendingChips()}</div>
-            )}
+            <div className="flex items-center gap-3">
+              {categoryButtons}
+              {hasTrending && (
+                <div className="flex items-center gap-3 ml-6">{renderTrendingChips()}</div>
+              )}
+            </div>
           </div>
         </div>
 
